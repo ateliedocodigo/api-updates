932ae7b27e57d4eefbb404d2d738808245f8772f
Luis Fernando Gomes <dev@luiscoms.com.br>
2019-06-28 15:22:07 -0300

    Updates

---
+++ precache-manifest.dc1be1ccbea91de3f964fc04cb79309a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5984ddb05b60c6dc7bee694606a337",
+    "url": "/api-updates/index.html"
+  },
+  {
+    "revision": "6dda57c07bf0db1e634e",
+    "url": "/api-updates/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "3d61423f7a3e5c25f60e",
+    "url": "/api-updates/static/css/main.09f9c0fa.chunk.css"
+  },
+  {
+    "revision": "6dda57c07bf0db1e634e",
+    "url": "/api-updates/static/js/2.ef134f78.chunk.js"
+  },
+  {
+    "revision": "3d61423f7a3e5c25f60e",
+    "url": "/api-updates/static/js/main.cefc175d.chunk.js"
+  },
+  {
+    "revision": "199f79a2b9f56e06e852",
+    "url": "/api-updates/static/js/runtime~main.4698f10c.js"
+  }
+]);
